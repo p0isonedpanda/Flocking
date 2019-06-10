fab3f22ff7ea1cfa5e84d4041ee2ac3fb2fa7990
Daniel Coady <daniel.coady@outlook.com.au>
2019-06-10 14:21:56 +1000

    initial commit

---
+++ Assets/Agent/AgentController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+	GameController gc = GameController.instance;
+
+    [SerializeField]
+	private int moveSpeed = 1;
+    [SerializeField]
+	private int neighbourhoodDistance = 10;
+	
+	void Update()
+	{
+		Quaternion wanderRot = Wander(transform.rotation);
+
+		Transform finalTransform = transform;
+		finalTransform.rotation = wanderRot;
+
+		transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		transform.rotation = finalTransform.rotation;
+
+        if (gc.debugging)
+		    foreach (GameObject a in GetNeighbours())
+		        Debug.DrawLine(transform.position, a.transform.position, Color.red);
+	}
+
+    // used for wandering
+	Quaternion GetNewRot()
+	{
+		return Quaternion.Euler
+		(
+			Random.Range(0, 360),
+			Random.Range(0, 360),
+			Random.Range(0, 360)
+		);
+	}
+
+	Quaternion Wander(Quaternion q)
+	{
+		q = Quaternion.RotateTowards(q, GetNewRot(), 10.0f);
+		return q;
+	}
+
+	IEnumerable<GameObject> GetNeighbours()
+	{
+		return gc.agents.Where(a => Vector3.Distance(a.transform.position, transform.position) <= neighbourhoodDistance);
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public static GameController instance { get; private set; }
+
+    public bool debugging = false;
+	[HideInInspector] public List<GameObject> agents;
+	[SerializeField] private GameObject agent;
+
+	void Awake()
+	{
+		if (instance != null)
+		    throw new System.Exception();
+		
+		instance = this;
+	}
+
+	void Start ()
+	{
+		agents = new List<GameObject>();
+
+		for (int i = 0; i < 500; i++)
+		{
+			Vector3 pos = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+			Quaternion rot = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+			agents.Add(Instantiate(agent, pos, rot));
+		}
+	}
+}
